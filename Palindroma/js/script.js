686ef6a45eb71c8e0f91fc4d5290fc52b149e3c5
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-06 14:43:42 +0100

    first commit

---
+++ Palindroma/js/script.js
@@ -0,0 +1,5 @@
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma
+// (ad esempio, questa funzione potrebbe funzionare così: passandole la parola come parametro, 
+// farà i suoi calcoli per capire se è palindroma o meno e ci restituirà true o false in base all'esito)
+
