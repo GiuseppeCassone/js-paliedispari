436eba1ab06b367034fb6ec4f00ac3fec76d900f
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-06 16:01:28 +0100

    palindrome folder completed

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -3,3 +3,32 @@
 // (ad esempio, questa funzione potrebbe funzionare così: passandole la parola come parametro, 
 // farà i suoi calcoli per capire se è palindroma o meno e ci restituirà true o false in base all'esito)
 
+const buttonEl = document.querySelector("button");
+
+buttonEl.addEventListener("click",
+function() {
+
+    // Chiedo all'utente di inserire una parola
+    let userWord = prompt("Inserisci una parola");
+    
+    // Inizializzo la funzione per il controllo della parola palindroma
+    function parolaPalindroma(word) {
+    
+        let i = 0;
+    
+    // Divido la parola in due per controllare le due metà
+        while (i < word.length / 2){
+    
+            if(word[i] !== word[word.length - 1 - i]){
+    
+                return false;
+            }
+    
+            i++;
+        }
+        return true;
+    }
+    
+    document.getElementById(`result`).innerHTML = parolaPalindroma(userWord);
+    
+})
